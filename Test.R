8103fe63628a2cf54f7becd5116290aedf269804
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-07-28 02:52:53 +0900

    Add citation sample

--- Test.R
+++ Test.R
@@ -4,6 +4,10 @@ library("BradleyTerry2")
 data("citations", package = "BradleyTerry2")
 citations
 citations.sf <- countsToBinomial(citations)
+names(citations.sf)[1:2] <- c("journal1", "journal2")
 
-citeModel <- Btm(cbind(win1, win2), journal1, journal2, ~ journal, id = "journal", data = citations.sf)
+citeModel <- BTm(cbind(win1, win2), journal1, journal2, ~ journal, id = "journal", data = citations.sf)
 citeModel
+
+summary(citeModel)
+BTabilities(citeModel)
